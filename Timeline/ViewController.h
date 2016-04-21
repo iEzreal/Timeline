060ed069b93ff57c32fc1456046dd1c6bfd6af02
iEzreal <747141623@qq.com>
2016-04-21 10:33:28 +0800

    Initial Commit

---
+++ Timeline/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Timeline
+//
+//  Created by Ezreal on 16/4/21.
+//  Copyright © 2016年 Ezreal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Timeline/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Timeline
+//
+//  Created by Ezreal on 16/4/21.
+//  Copyright © 2016年 Ezreal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
